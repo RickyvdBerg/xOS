e686c341d6d62d51ce06b6da48629a5ac81449f7
rvdb <ricky.vandenberg@hotmail.nl>
2025-06-05 16:10:21 +0200

    t

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,31 +4,83 @@ set -oue pipefail
 
 # Install dependencies
 rpm-ostree install gtk-murrine-engine gtk2-engines kvantum qt5ct qt6ct \
-    gnome-tweaks git sassc
+    gnome-tweaks git sassc gnome-themes-extra
 
 # Install Colloid from source
 echo "Installing Colloid GTK Theme..."
 curl -L https://github.com/vinceliuice/Colloid-gtk-theme/archive/refs/heads/master.zip -o /tmp/colloid.zip
 unzip /tmp/colloid.zip -d /tmp
 
-# Add verbosity to the theme's install.sh script
-THEME_INSTALL_SCRIPT="/tmp/Colloid-gtk-theme-main/install.sh"
-sed -i '1a set -x' "$THEME_INSTALL_SCRIPT"
+# Define source directories from the unzipped theme
+COLLOID_SRC_ROOT="/tmp/Colloid-gtk-theme-main"
+THEME_SRC_DIR="${COLLOID_SRC_ROOT}/src"
+SASS_SRC_DIR="${THEME_SRC_DIR}/sass"
+MAIN_SRC_DIR="${THEME_SRC_DIR}/main"
 
 # Set environment for non-interactive installation
 export DEBIAN_FRONTEND=noninteractive
 export HOME=/tmp/build-home # Safety for any script part trying to write to $HOME
 mkdir -p $HOME
-mkdir -p /root/.config /root/.local/share/themes /root/.themes /root/.gnupg # For GPG and other potential user-dir needs if HOME is ignored
+mkdir -p /root/.config /root/.local/share/themes /root/.themes /root/.gnupg # Precaution
 
-# Install Colloid theme system-wide for dark and light variants
-echo "Installing Colloid Dark theme system-wide (with internal script tracing enabled)..."
-"$THEME_INSTALL_SCRIPT" -d /usr/share/themes -n Colloid -t gtk3 gtk4 gnome-shell -c dark
-echo "Installing Colloid Light theme system-wide (with internal script tracing enabled)..."
-"$THEME_INSTALL_SCRIPT" -d /usr/share/themes -n Colloid -t gtk3 gtk4 gnome-shell -c default
+# --- Install Colloid-Dark Theme ---
+THEME_NAME_DARK="Colloid-Dark"
+DEST_DIR_DARK="/usr/share/themes/${THEME_NAME_DARK}"
+
+echo "Preparing to install ${THEME_NAME_DARK}..."
+mkdir -p "${DEST_DIR_DARK}/gtk-3.0" \
+           "${DEST_DIR_DARK}/gtk-4.0" \
+           "${DEST_DIR_DARK}/gnome-shell" \
+           "${DEST_DIR_DARK}/xfwm4" \
+           "${DEST_DIR_DARK}/cinnamon" \
+           "${DEST_DIR_DARK}/plank" \
+           "${DEST_DIR_DARK}/metacity-1"
+
+echo "Setting up SCSS links for ${THEME_NAME_DARK}..."
+ln -sf "${SASS_SRC_DIR}/_colors-dark.scss" "${SASS_SRC_DIR}/_colors.scss"
+ln -sf "${SASS_SRC_DIR}/_drawing-dark.scss" "${SASS_SRC_DIR}/_drawing.scss"
+
+echo "Compiling SCSS for ${THEME_NAME_DARK}..."
+sassc "${MAIN_SRC_DIR}/gtk-3.0/gtk.scss" "${DEST_DIR_DARK}/gtk-3.0/gtk.css"
+sassc "${MAIN_SRC_DIR}/gtk-3.0/gtk-dark.scss" "${DEST_DIR_DARK}/gtk-3.0/gtk-dark.css"
+sassc "${MAIN_SRC_DIR}/gtk-4.0/gtk.scss" "${DEST_DIR_DARK}/gtk-4.0/gtk.css"
+sassc "${MAIN_SRC_DIR}/gtk-4.0/gtk-dark.scss" "${DEST_DIR_DARK}/gtk-4.0/gtk-dark.css"
+sassc "${MAIN_SRC_DIR}/gnome-shell/gnome-shell.scss" "${DEST_DIR_DARK}/gnome-shell/gnome-shell.css"
+
+echo "Installing assets for ${THEME_NAME_DARK}..."
+bash "${COLLOID_SRC_ROOT}/assets.sh" -d "/usr/share/themes" -n "${THEME_NAME_DARK}" -c dark
+echo "Installing gtkrc for ${THEME_NAME_DARK}..."
+bash "${COLLOID_SRC_ROOT}/gtkrc.sh" -d "/usr/share/themes" -n "${THEME_NAME_DARK}" -c dark
+
+# --- Install Colloid Light Theme (as 'Colloid') ---
+THEME_NAME_LIGHT="Colloid"
+DEST_DIR_LIGHT="/usr/share/themes/${THEME_NAME_LIGHT}"
+
+echo "Preparing to install ${THEME_NAME_LIGHT}..."
+mkdir -p "${DEST_DIR_LIGHT}/gtk-3.0" \
+           "${DEST_DIR_LIGHT}/gtk-4.0" \
+           "${DEST_DIR_LIGHT}/gnome-shell" \
+           "${DEST_DIR_LIGHT}/xfwm4" \
+           "${DEST_DIR_LIGHT}/cinnamon" \
+           "${DEST_DIR_LIGHT}/plank" \
+           "${DEST_DIR_LIGHT}/metacity-1"
+
+echo "Setting up SCSS links for ${THEME_NAME_LIGHT}..."
+ln -sf "${SASS_SRC_DIR}/_colors-light.scss" "${SASS_SRC_DIR}/_colors.scss"
+ln -sf "${SASS_SRC_DIR}/_drawing-light.scss" "${SASS_SRC_DIR}/_drawing.scss"
+
+echo "Compiling SCSS for ${THEME_NAME_LIGHT}..."
+sassc "${MAIN_SRC_DIR}/gtk-3.0/gtk.scss" "${DEST_DIR_LIGHT}/gtk-3.0/gtk.css"
+sassc "${MAIN_SRC_DIR}/gtk-4.0/gtk.scss" "${DEST_DIR_LIGHT}/gtk-4.0/gtk.css"
+sassc "${MAIN_SRC_DIR}/gnome-shell/gnome-shell.scss" "${DEST_DIR_LIGHT}/gnome-shell/gnome-shell.css"
+
+echo "Installing assets for ${THEME_NAME_LIGHT}..."
+bash "${COLLOID_SRC_ROOT}/assets.sh" -d "/usr/share/themes" -n "${THEME_NAME_LIGHT}" -c default
+echo "Installing gtkrc for ${THEME_NAME_LIGHT}..."
+bash "${COLLOID_SRC_ROOT}/gtkrc.sh" -d "/usr/share/themes" -n "${THEME_NAME_LIGHT}" -c default
 
 # Clean up
-rm -rf /tmp/Colloid-gtk-theme-main /tmp/colloid.zip $HOME
+rm -rf "${COLLOID_SRC_ROOT}" /tmp/colloid.zip $HOME
 
 # Install Colloid Icon Theme from source
 echo "Installing Colloid Icon Theme..."
